89c4cdaa60e79633fb67ba61f9d1ee9f475c7609
Ledo Ivo Veras <ledoivoveras@gmail.com>
2024-06-02 14:26:26 -0300

    sql updated

--- src/main/resources/db/migration/V01__create-table-pessoas.sql
+++ src/main/resources/db/migration/V01__create-table-pessoas.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Pessoas
 (
     id          BIGINT       NOT NULL AUTO_INCREMENT,
-    nome        VARCHAR(100) NOT NULL,
+    nome        VARCHAR(40) NOT NULL,
     ativo       BOOLEAN      NOT NULL DEFAULT TRUE,
     logradouro  VARCHAR(100) NOT NULL,
     bairro      VARCHAR(100) NOT NULL,
     cep         VARCHAR(9)   NOT NULL,
     complemento VARCHAR(100),
-    numero      VARCHAR(20),
+    numero      VARCHAR(10),
     uf          CHAR(2)      NOT NULL,
-    cidade      VARCHAR(100) NOT NULL,
+    cidade      VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
